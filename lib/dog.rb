4ec5a5dd97121efa0dc5e6f1a1845dd515ecf051
Rafael Lopez <lpz.raf@gmail.com>
2018-11-28 00:32:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,8 +40,7 @@ class Dog
   end
   
   def self.find_or_create_by(name: , breed:)
-    sql = "" 
-    DB[:conn].execute(sql)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = '#{name}' AND breed = '#{breed}')
   end
   
   
